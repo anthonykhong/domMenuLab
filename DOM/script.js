cf31c84ac83af90273a81a37cf6b57ad85cb0422
anthonykhong <anthonykhong95200@gmail.com>
2023-02-25 02:04:15 +0100

    Task 1

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "gray";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList = "flex-ctr";
+
+const topMenuEl = document.querySelector("nav #top-menu");
