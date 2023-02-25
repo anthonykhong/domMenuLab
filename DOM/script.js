39544c6cd91962e35cd50c698b622601875a14e4
anthonykhong <anthonykhong95200@gmail.com>
2023-02-25 22:57:45 +0100

    Task 3

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+const menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
@@ -7,3 +15,11 @@ const topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.background = "var(--top-menu-bg)";
 topMenuEl.classList = "flex-around";
+
+for (let i = 0; i < menuLinks.length; i++) {
+  const link = menuLinks[i];
+  const linkEl = document.createElement("a");
+  linkEl.href = link.href;
+  linkEl.textContent = link.text;
+  topMenuEl.appendChild(linkEl);
+}
