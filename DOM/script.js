4f4cd8e89728e7aa7d0c730d56eb1c068db9526d
anthonykhong <anthonykhong95200@gmail.com>
2023-02-25 21:18:12 +0100

    Pt 1 Task 2

--- DOM/script.js
+++ DOM/script.js
@@ -3,4 +3,7 @@ mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 mainEl.classList = "flex-ctr";
 
-const topMenuEl = document.querySelector("nav #top-menu");
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.background = "var(--top-menu-bg)";
+topMenuEl.classList = "flex-around";
